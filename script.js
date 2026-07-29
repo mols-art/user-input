1ebeb13e313cd92e553ba93691749fe69a9b22ee
mols-art <mariogomez005@outlook.com>
2026-07-29 11:20:25 -0700

    first commit

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 // get references
+let textbox = document.getElementById('boxtext');
+let clickButton = document.querySelector('button');
+let screenDiv = document.querySelector('div');
+let bodyRef = document.querySelector('body');
 
 // set up onclick for clickButton
-
+clickButton.onclick = function (event) {
 // prevent the page from reloading
-
+event.preventDefault();
 // capture user input
-
+let userInput = textbox.value;
+console.log(userInput);
 // change backgroundColor based on userInput
-
+bodyRef.style.backgroundColor = userInput;
 // show userInput on the screen
-
+screenDiv.innerHTML = userInput;
 // Clear input
+textbox.value = '';
+}
